fdc4935c972fbb90d146ce5b9458221dfd17d072
mariaeduarda2504 <mariaeduardad391@gmail.com>
2022-11-18 08:10:09 -0300

    React

--- src/services/dados.ts
+++ src/services/dados.ts
@@ -1,5 +1,102 @@
 const dados = [
-
-]
-
-export default dados
+    {
+      id: 1,
+      nome: "Maria Eduarda",
+      data: "27/04/2022 10:08:00",
+      titulo: "Curso",
+      mensagem: "Tenho um curso a oferecer!",
+      topico: [{
+        id: 1,
+        item: "Estudar"
+      }, {
+        id: 2,
+        item: "Escolher curso"
+      }]
+    },
+    {
+      id: 2,
+      nome: "Gabi",
+      data: "28/04/2022 10:08:00",
+      titulo: "Curso",
+      mensagem: "Tenho um curso a oferecer!",
+      topico: [{
+        id: 1,
+        item: "Estudar"
+      }, {
+        id: 2,
+        item: "Escolher curso"
+      }]
+    },
+    {
+      id: 3,
+      nome: "Luana",
+      data: "28/04/2022 10:08:00",
+      titulo: "Curso",
+      mensagem: "Tenho um curso a oferecer!",
+      topico: [{
+        id: 1,
+        item: "Estudar"
+      }, {
+        id: 2,
+        item: "Escolher curso"
+      }]
+    },
+    {
+      id: 4,
+      nome: "Mary",
+      data: "28/04/2022 10:08:00",
+      titulo: "Curso",
+      mensagem: "Tenho um curso a oferecer!",
+      topico: [{
+        id: 1,
+        item: "Estudar"
+      }, {
+        id: 2,
+        item: "Escolher curso"
+      }]
+    },
+    {
+      id: 5,
+      nome: "Samid",
+      data: "28/04/2022 10:08:00",
+      titulo: "Curso",
+      mensagem: "Tenho um curso a oferecer!",
+      topico: [{
+        id: 1,
+        item: "Estudar"
+      }, {
+        id: 2,
+        item: "Escolher curso"
+      }]
+    },
+    {
+      id: 6,
+      nome: "Julya",
+      data: "28/04/2022 10:08:00",
+      titulo: "Curso",
+      mensagem: "Tenho um curso a oferecer!",
+      topico: [{
+        id: 1,
+        item: "Estudar"
+      }, {
+        id: 2,
+        item: "Reciclar"
+      }]
+    },
+    {
+      id: 7,
+      nome: "Andressa",
+      data: "28/04/2022 10:08:00",
+      titulo: "Curso",
+      mensagem: "Tenho um curso a oferecer",
+      topico: [{
+        id: 1,
+        item: "Estudar"
+      }, {
+        id: 2,
+        item: "Escolher curso"
+      }]
+    },
+  ]
+  
+  export default dados
